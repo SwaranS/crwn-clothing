ea06514783c0a1561a11102ce4ec213a8d1718c8
Swaran <singhsw@tcd.ie>
2021-02-14 16:18:53 +0000

    Added menu Items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
 import HomePage from "./componenets/pages/homemage.component";
 import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
